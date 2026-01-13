26f6e86311b81e21da934eef127c8541ec29340b
RVS <rvspromotion@gmail.com>
2026-01-13 08:59:38 +0100

    Delete I9 Batch directory

--- I9 Batch/__init__.py
+++
@@ -1,6 +0,0 @@
-from .i9_batch_processing import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-# Tell ComfyUI this node has web extensions
-WEB_DIRECTORY = "./web"
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
