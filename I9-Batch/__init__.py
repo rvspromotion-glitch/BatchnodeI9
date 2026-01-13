e76be327c2817f055180d52e9ae35f11d329bb8c
RVS <rvspromotion@gmail.com>
2026-01-13 08:57:34 +0100

    Create __init__.py

---
+++ I9-Batch/__init__.py
@@ -0,0 +1,6 @@
+from .i9_batch_processing import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+# Tell ComfyUI this node has web extensions
+WEB_DIRECTORY = "./web"
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
